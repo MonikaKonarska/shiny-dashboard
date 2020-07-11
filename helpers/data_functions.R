a75769b3b44341f6d7f9173a638de6b4b119ca3d
monikakkk <monika.konarska63@gmail.com>
2020-07-11 09:12:06 +0200

    add function to compute values for infoboxes

--- helpers/data_functions.R
+++ helpers/data_functions.R
@@ -15,3 +15,22 @@ download_data_from_website <- function(url){
   data <- read.csv(textConnection(get_url_data), encoding = "UTF-8", stringsAsFactors=FALSE)
   return(data)
 }
+
+
+
+compute_statistic_to_infoBox <- function(dataset, variable_name, type_statistic = c("mean", "median", "count", "count_distinct")){
+  statistic <- match.arg(type_statistic)
+  
+  if(statistic == "count"){
+    value <- nrow(dataset)
+  } else if (statistic == "count_distinct") {
+    value <- length(dataset[, unique(get(variable_name))])
+  } else {
+  value <- dataset[, get(statistic)(get(variable_name))]
+  }
+  return(value)
+}
+
+
+
+
