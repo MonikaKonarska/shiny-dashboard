da7ce687041735e548a268ce9ebd043634f1a530
monikakkk <monika.konarska63@gmail.com>
2020-07-11 21:50:14 +0200

    add more breaks on x axis

--- helpers/plot_functions.R
+++ helpers/plot_functions.R
@@ -12,10 +12,11 @@ number_of_rentals_plot <- function(dataset, x_variable_name = "start_date") {
 
   p <- ggplot(data, aes(x = !!x, y = number_rentals)) +
     geom_line(color = "steelblue", size = 1) +
-    scale_x_date(date_labels = "%B")+
+    scale_x_date(date_labels = "%b %d", date_breaks = "10 days", position = "bottom")+
     scale_y_continuous(name = "Amount of rents", labels = scales::comma)+
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
-  
+    theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+          axis.text.y = element_text(size = 10))
+
   return(p)
 }
 
