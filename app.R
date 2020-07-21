c44f27be4e2517ba2e044d2443ddb7b86c2f1fe1
monikakkk <monika.konarska63@gmail.com>
2020-07-21 22:38:25 +0200

    add modules related with maps and tables

--- app.R
+++ app.R
@@ -6,6 +6,9 @@ library(scales)
 
 source(file.path(getwd(), "modules", "data_modules.R"))
 source(file.path(getwd(), "modules", "plot_modules.R"))
+source(file.path(getwd(), "modules", "table_modules.R"))
+source(file.path(getwd(), "modules", "map_modules.R"))
+
 
 ui <- dashboardPage(
   
@@ -26,19 +29,50 @@ ui <- dashboardPage(
               fluidRow(
                 filteringDatasetModuleUI("select_data"),
                 br(),
-                column(width = 7, plotOutput(outputId = "amount_rents_plot") %>% withSpinner(color = "#0dc5c1"))
+                column(width = 7, plotOutput(outputId = "amount_rents_plot") %>% withSpinner(color = "#0dc5c1", size = 0.75))
                 ),
-              peakHoursModuleUI("peak_hours")
-              )
+              h1("Peak Rental Season", align = "center"),
+              tabsetPanel(type = "pills",
+                          tabPanel("Hours rents",
+                                   peakRentalModuleUI("generateplos_hours")
+                                   ),
+                          tabPanel("Performance indicators",
+                                   peakMonthsIndicatorsModuleUI("agreagted_month_ind_plot")),
+                          tabPanel(
+                                   peakPercentagesIndicatorsModuleUI("agregated_percentage_ind_plot")
+                                   )
+                                   
+                                   )
+                          
+              ),
+      tabItem(tabName = "bike_stations" , 
+              createMainMapsModuleUI("main_maps")
+      
+        
       )
-    )
-  )
+  )))
 
 server <- function(input, output, session) {
   filtered_data  <- callModule(module = filteringDatasetModule, id = "select_data")
-  callModule(module = infoBoxModuleServer , id = "infoboxes",  dataset = filtered_data)
-  callModule(module = peakHoursModuleServer, id = "peak_hours", dataset = filtered_data)
+  agregated_data_byhours <- callModule(module = peakRentalDataModuleServer, id = "peak_hours", dataset = filtered_data)
+  agregated_data_ind_month <- callModule(module = peakDataMonthsIndicatorsModuleServer, id = "agregated_month_ind", dataset = filtered_data)
+  agregated_data_percentage <- callModule(module = peakDataPercentagesIndicatorsServer, id = "agregated_percentage_id", dataset = filtered_data)
   
+  callModule(module = infoBoxModuleServer , id = "infoboxes",  dataset = filtered_data)
+  callModule(module = peakHoursPlotsModuleServer, id = "generateplos_hours", dataset = agregated_data_byhours)
+  callModule(module = peakMonthsIndicatorsModuleServer, id = "agreagted_month_ind_plot", dataset = agregated_data_ind_month)
+  callModule(module = peakPercentagesIndicatorsServer, id = "agregated_percentage_ind_plot", dataset = agregated_data_percentage)
+  
+  stations <- callModule(module = prepareDataStationsToFirstMapsServer, id = "stations_id")
+  districts <- callModule(module = prepareDataDistrictsToFirstMapsServer, id = "districts_id")
+  callModule(module = createMainMapsServer, id = "main_maps", data_stations = stations, data_districts = districts)
+  
+  
+  
+  
+  
+  
+ # callModule(module = peakMonthsIndicatorsModuleServer, id = "indicators", dataset = filtered_data)
   
   output$amount_rents_plot <- renderPlot({
     number_of_rentals_plot(dataset = filtered_data())
