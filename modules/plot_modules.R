40df9be76fbfbf9d7ff7ff2cd8396163209c2b23
monikakkk <monika.konarska63@gmail.com>
2020-07-21 22:36:40 +0200

    add title to dzis module,

--- modules/plot_modules.R
+++ modules/plot_modules.R
@@ -5,42 +5,41 @@ library(data.table)
 custom.col <- c( "#E69F00", "#56B4E9", "#009E73",
                  "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-peakHoursModuleUI <- function(id) {
+peakRentalModuleUI <- function(id) {
   ns <- NS(id)
   tagList(
-    h1("Peak hours", align = "center"),
     fluidRow(
       column(width = 12,
-             box(plotOutput(ns("hours_rent_plot")), width = 6),
-             box(plotOutput(ns("hours_rent_days_plot")), width = 6)
-             )
-      )
+             box(plotOutput(ns("hours_rent_plot")) %>% withSpinner(color = "#0dc5c1", size = 0.75), width = 6),
+             box(plotOutput(ns("hours_rent_days_plot")) %>% withSpinner(color = "#0dc5c1", size = 0.75), width = 6)
+             ))
     )
   }
 
 
-peakHoursModuleServer <- function(input, output, session, dataset) {
-  output$hours_rent_plot <- renderPlot({
-  grouped_data <- dataset()[, .(n = .N), keyby = .(day_rent, hour_rent_start)][, freq := prop.table(n), by = "day_rent" ]
-  ggplot(grouped_data, aes(x = hour_rent_start, y = freq,  fill = day_rent)) +
-    geom_bar(stat = "identity")+
-    scale_y_continuous(labels = percent)+
-    scale_x_continuous(breaks = scales::pretty_breaks(n = 15), expand = c(0, 0))+
-    labs( y = "Frequency of rents", x = "Hour rent start", fill = "Day")+
-    scale_fill_manual(values = custom.col)
+peakRentalDataModuleServer <- function(input, output, session, dataset) {
+  agregated_data_byhours <- reactive({
+    dataset()[, .(n = .N), keyby = .(day_rent, hour_rent_start)][, freq := prop.table(n), by = "day_rent"]
   })
-  
+  return(agregated_data_byhours)
+  }
+
+peakHoursPlotsModuleServer <- function(input, output, session, dataset){
+  output$hours_rent_plot <- renderPlot({
+    ggplot(dataset(), aes(x = hour_rent_start, y = freq,  fill = day_rent)) +
+      geom_bar(stat = "identity")+
+      scale_y_continuous(labels = percent)+
+      scale_x_continuous(breaks = scales::pretty_breaks(n = 15), expand = c(0, 0))+
+      labs( y = "Frequency of rents", x = "Hour rent start", fill = "Day")+
+      scale_fill_manual(values = custom.col)
+  })
   output$hours_rent_days_plot <- renderPlot({
-    grouped_data <- dataset()[, .(n = .N), keyby = .(day_rent, hour_rent_start)][, freq := prop.table(n), by = "day_rent"]
-    ggplot(grouped_data, aes(x = hour_rent_start, y = freq )) +
+    ggplot(dataset(), aes(x = hour_rent_start, y = freq )) +
       geom_bar(stat = "identity")+
       scale_y_continuous(labels = percent)+
       facet_wrap(~day_rent, nrow = 4)
-    })
-  }
-
-
-
+  })
+}
 
 
 
